8f67af9b15bd512cdbb033db33a13b7e1e788d9a
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-07-29 16:12:12 +0200

    added post parser draft

---
+++ include/balde-blog.h
@@ -0,0 +1,13 @@
+/*
+ * balde-blog: A really simple blog application built with balde.
+ * Copyright (C) 2014 Rafael G. Martins <rafael@rafaelmartins.eng.br>
+ *
+ * This program can be distributed under the terms of the LGPL-2 License.
+ * See the file COPYING.
+ */
+
+#ifndef _BALDE_BLOG_H
+#define _BALDE_BLOG_H
+
+
+#endif /* _BALDE_BLOG_H */
---
+++ libbalde_blog/post-parser.c
@@ -0,0 +1,15 @@
+/*
+ * balde-blog: A really simple blog application built with balde.
+ * Copyright (C) 2014 Rafael G. Martins <rafael@rafaelmartins.eng.br>
+ *
+ * This program can be distributed under the terms of the LGPL-2 License.
+ * See the file COPYING.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <glib.h>
+
+#include <balde-blog/post-parser-private.h>
